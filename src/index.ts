c2a100a5511e275f88b272444511e1f0c5d097c2
Norman Vasquez <nvasquez@serviciosap.com>
2019-08-04 13:22:03 -0600

    getting the tournament dates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as timezone from "moment-timezone";
 import { Tournament } from "./tournament/models/tournament";
 
 fs.readFile(
@@ -8,26 +9,32 @@ fs.readFile(
     if (error) throw error;
     let tournamentStringArray = data.split("\nPokerStars Tournament");
     tournamentStringArray.shift();
-    tournamentStringArray.forEach(content => {
-      console.log(
-        content
-          .split("\nTournament started ")[1]
-          .split("\n")[0]
-          .replace(/[ET]/g, "")
-      );
-      console.log(
-        new Date(
-          content
-            .split("\nTournament started ")[1]
-            .split("\n")[0]
-            .replace(/[ET]/g, "")+"-0500"
-        )
-      );
+    tournamentStringArray.forEach((content, index) => {
+      console.log(content);
     });
     /* let tournaments = tournamentStringArray.map<Tournament>(content=>{
     })*/
   }
 );
+
+function getSartDate(content: string): Date {
+  let string = content
+    .split("\nTournament started ")[1]
+    .split("\n")[0]
+    .replace(/[ET]/g, "");
+  return getPokerStarsDate(string);
+}
+
+function getEndDate(content: string): Date | null {
+  let key = "\nTournament finished ";
+  if (!content.includes(key)) return null;
+  let string = content
+    .split(key)[1]
+    .split("\n")[0]
+    .replace(/[ET]/g, "");
+  console.log(string);
+  return getPokerStarsDate(string);
+}
 
 function getTournamentId(content: string): number {
   return Number.parseInt(content.split(" #")[1].split(",")[0]);
@@ -58,4 +65,10 @@ function getPrizePool(content: string): number {
       .split(" ")[0]
       .replace(/[$]/g, "")
   );
+}
+
+function getPokerStarsDate(dateStringWithOutEt: string): Date {
+  return timezone
+    .tz(dateStringWithOutEt, "YYYY/MM/DD HH:mm:ss", "America/New_York")
+    .toDate();
 }
